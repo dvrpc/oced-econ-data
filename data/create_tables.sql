0701a5b84a714829b27ab57b6912723684550eb8
Kris Warner <kdwarn@protonmail.com>
2022-04-06 14:09:33 -0400

    Add file to create tables (and types)

---
+++ data/create_tables.sql
@@ -0,0 +1,70 @@
+/* Use psql: psql -f /path/to/this/file -d name_of_database (and potentially user, port, localhost
+depending on setup)
+*/
+
+BEGIN;
+
+
+DO $$ BEGIN
+    CREATE TYPE geographic_area AS ENUM (
+        'United States',
+        'DVRPC Region',
+        'Philadelphia MSA', 
+        'Trenton MSA'
+    );
+EXCEPTION
+    WHEN duplicate_object THEN null;
+    raise notice 'type "geographic_area" already exists, skipping';
+END $$;
+
+DO $$ BEGIN
+    CREATE TYPE industry_group AS ENUM (
+        'Mining, logging, and construction',
+        'Manufacturing',
+        'Trade, transportation, and utilities',
+        'Information',
+        'Financial activities',
+        'Professional and business services',
+        'Education and health services',
+        'Leisure and hospitality',
+        'Other services',
+        'Government',
+        'Total'
+    );
+EXCEPTION
+    WHEN duplicate_object THEN null;
+    raise notice 'type "industry_group" already exists, skipping';
+END $$;
+
+CREATE TABLE IF NOT EXISTS inflation_rate (
+    id SERIAL,
+    period DATE NOT NULL,
+    rate REAL NOT NULL,
+    area geographic_area NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS unemployment_rate (
+    id SERIAL,
+    period DATE NOT NULL,
+    rate REAL NOT NULL,
+    area geographic_area NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS housing (
+    id SERIAL,
+    period DATE NOT NULL,
+    units SMALLINT,
+    rate REAL NOT NULL,
+    area geographic_area NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS employment_by_industry (
+    id SERIAL,
+    period DATE NOT NULL,
+    number REAL NOT NULL,
+    share REAL NOT NULL,
+    industry industry_group NOT NULL,
+    area geographic_area NOT NULL
+);
+
+COMMIT;
