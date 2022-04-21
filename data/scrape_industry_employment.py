397ec7afc5c1b14a18f7f3238c54d0067821d948
Kris Warner <kdwarn@protonmail.com>
2022-04-21 15:22:07 -0400

    Exit early if request fails

--- data/scrape_industry_employment.py
+++ data/scrape_industry_employment.py
@@ -1,9 +1,14 @@
+import sys
+
 from bs4 import BeautifulSoup
 import requests
 
 url_to_scrape = "https://www.bls.gov/regions/mid-atlantic/data/xg-tables/ro3fx9527.htm"
 
 r = requests.get(url_to_scrape)
+
+if r.status_code != 200:
+    sys.exit(f"Unable to get {url_to_scrape}")
 
 soup = BeautifulSoup(r.text, features="html.parser")
 
