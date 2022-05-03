ed4f5891c71da10722324c74ecbc449ea3c0e3ce
brcarney <bcarney1919@gmail.com>
2022-05-03 13:54:56 -0400

    Updated housing.py

--- data/housing.py
+++ data/housing.py
@@ -6,16 +6,33 @@ import pandas as pd
 import numpy as np
 
 
+header_list = ["Date", "State_FIPS", "County_FIPS", "Region_Code", "Division_Code", "County", "buildings_1unit", "units_1unit", "permitVal_1unit", "buildings_2units", "units_2units", "permitVal_2units", "buildings_3to4units", "units_3to4units", "permitVal_3to4units", "buildings_5punits", "units_5punits", "permitVal_5punits", "rep_buildings_1unit", "rep_units_1unit", "rep_permitVal_1unit", "rep_buildings_2units", "rep_units_2units", "rep_permitVal_2units", "rep_buildings_3to4units", "rep_units_3to4units", "rep_permitVal_3to4units", "rep_buildings_5punits", "rep_units_5punits", "rep_permitVal_5punits"]
+
+mar22_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2203c.txt", dtype={'FIPS': str}, names= header_list)
+feb22_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2202c.txt", dtype={'FIPS': str}, names= header_list)
+jan22_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2201c.txt", dtype={'FIPS': str}, names= header_list)
+dec21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2112c.txt", dtype={'FIPS': str}, names= header_list)
+nov21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2111c.txt", dtype={'FIPS': str}, names= header_list)
+oct21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2110c.txt", dtype={'FIPS': str}, names= header_list)
+sep21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2109c.txt", dtype={'FIPS': str}, names= header_list)
+aug21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2108c.txt", dtype={'FIPS': str}, names= header_list)
+jul21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2107c.txt", dtype={'FIPS': str}, names= header_list)
+jun21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2106c.txt", dtype={'FIPS': str}, names= header_list)
+may22_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2105c.txt", dtype={'FIPS': str}, names= header_list)
+apr22_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2104c.txt", dtype={'FIPS': str}, names= header_list)
+mar21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2103c.txt", dtype={'FIPS': str}, names= header_list)
+feb21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2102c.txt", dtype={'FIPS': str}, names= header_list)
+jan21_month = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2101c.txt", dtype={'FIPS': str}, names= header_list)
+
+
+# Concatenate DFs
+df_list = [mar22_month, feb22_month,jan22_month,dec21_month,nov21_month,oct21_month,sep21_month,aug21_month,jul21_month,jun21_month,may22_month,apr22_month,mar21_month,feb21_month,jan21_month]
+df = pd.concat(df_list)
+print(df)
+
+
 #Region FIPS
 regionFIPS = ["34005", "34007", "34015", "34021", "42017", "42029", "42045", "42091", "42101"]
-
-annual_header_list = ["Date", "State_FIPS", "County_FIPS", "Region_Code", "Division_Code", "County", "buildings_1unit", "units_1unit", "permitVal_1unit", "buildings_2units", "units_2units", "permitVal_2units", "buildings_3to4units", "units_3to4units", "permitVal_3to4units", "buildings_5punits", "units_5punits", "permitVal_5punits", "rep_buildings_1unit", "rep_units_1unit", "rep_permitVal_1unit", "rep_buildings_2units", "rep_units_2units", "rep_permitVal_2units", "rep_buildings_3to4units", "rep_units_3to4units", "rep_permitVal_3to4units", "rep_buildings_5punits", "rep_units_5punits", "rep_permitVal_5punits"]
-
-raw21_annual = pd.read_csv(r"https://www2.census.gov/econ/bps/County/co2021a.txt", dtype={'FIPS': str}, names=annual_header_list)
-print(raw21_annual)
-
-raw20_annual = pd.read_csv("G:\\Shared drives\\Community & Economic Development\\EconomicUpdate\\data\\counties\\BuildingPermitsSurvey\\annual\\co2020a.txt", dtype={'FIPS': str}, names=annual_header_list)
-print(raw20_annual)
 
 """
 ToDo: 
