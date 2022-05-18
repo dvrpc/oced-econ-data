5d2c31e780dd7430e7676e8656ca499182d74f86
Kris Warner <kdwarn@protonmail.com>
2022-05-18 13:55:35 -0400

    Name things more precisely, simplify vars

--- data/housing.py
+++ data/housing.py
@@ -2,7 +2,7 @@
 Combine county-level authorized housing units into region-wide total from monthly data provided
 via text files by U.S. Dept. of Census.
 
-If --csv is passed to the program (python3 unemployment.py --csv), it will create a CSV of
+If --csv is passed to the program (python3 housing.py --csv), it will create a CSV of
 the fetched data. Otherwise, it will insert it into the database specified in the PG_CREDS
 variable in config.py.
 """
@@ -49,7 +49,7 @@ for row in table.find_all("tr"):
 
 county_files = county_files[:36]
 
-reg_header_list = [
+header_list = [
     "Date",
     "State_FIPS",
     "County_FIPS",
@@ -82,30 +82,36 @@ reg_header_list = [
     "rep_permitVal_5punits",
 ]
 
-reg_df_list = []
+all_counties_df_list = []
 
 for file in county_files:
-    reg_df_list.append(
+    all_counties_df_list.append(
         pd.read_csv(
             "https://www2.census.gov/econ/bps/County/" + file,
             dtype={
                 "State_FIPS": str,
                 "County_FIPS": str,
-                "units_1unit": float,
-                "units_2units": float,
-                "units_3to4units": float,
-                "units_5punits": float,
+                "units_1unit": int,
+                "units_2units": int,
+                "units_3to4units": int,
+                "units_5punits": int,
             },
-            names=reg_header_list,
+            names=header_list,
             low_memory=False,
             skiprows=[0, 1],
         )
     )
 
-reg_df = pd.concat(reg_df_list)
-reg_df["FIPS"] = reg_df["State_FIPS"] + reg_df["County_FIPS"]
-regionFIPS = ["34005", "34007", "34015", "34021", "42017", "42029", "42045", "42091", "42101"]
-region = reg_df.loc[reg_df["FIPS"].isin(regionFIPS)]
+all_counties = pd.concat(all_counties_df_list)
+all_counties["FIPS"] = all_counties["State_FIPS"] + all_counties["County_FIPS"]
+region = all_counties.loc[
+    all_counties["FIPS"].isin(
+        ["34005", "34007", "34015", "34021", "42017", "42029", "42045", "42091", "42101"]
+    )
+]
+
+pd.set_option("mode.chained_assignment", None)  # to ignore the copy warning in next step
+
 region["total_units"] = (
     region["units_1unit"]
     + region["units_2units"]
@@ -113,10 +119,10 @@ region["total_units"] = (
     + region["units_5punits"]
 )
 region = region[["Date", "total_units"]]
-region_grouped = region.groupby("Date").sum()
+region = region.groupby("Date").sum()
 
 data = []
-for row in region_grouped.itertuples():
+for row in region.itertuples():
     data.append([date.fromisoformat((str(row[0])[:4] + "-" + str(row[0])[4:] + "-01")), row[1]])
 
 # enter into db or create CSV
