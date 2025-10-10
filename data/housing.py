caf44396e76fe82c0a7e88dc51f588dbfa5d72c6
Kris Warner <kris@kdwarn.net>
2025-10-10 16:53:36 -0400

    Rm pandas for housing, handle errors at line level

--- data/housing.py
+++ data/housing.py
@@ -10,14 +10,16 @@ variable in config.py.
 import argparse
 import csv
 from datetime import date
+import logging
 from pathlib import Path
 import sys
 
 from bs4 import BeautifulSoup
-import pandas as pd
 import psycopg
 import requests
 import urllib3
+
+logger = logging.getLogger()
 
 # Disable warnings about unverified https requests.
 urllib3.disable_warnings()
@@ -26,13 +28,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--csv", action="store_true")
 args = parser.parse_args()
 
-# we don't need a database connection if just trying to create a CSV
+# We don't need a database connection if just trying to create a CSV.
 if args.csv:
     PG_CREDS = ""
 else:
     from config import PG_CREDS
 
-# get the county data filenames
+# Get the county data filenames.
 url_to_scrape = "https://www2.census.gov/econ/bps/County/?C=N;O=D"
 r = requests.get(url_to_scrape, verify=False)
 
@@ -50,85 +52,53 @@ for row in table.find_all("tr"):
             if cell.a.string.endswith("c.txt"):
                 county_files.append(cell.a.string)
 
+# Limit to last 3 years of files.
 county_files = county_files[:36]
 
-header_list = [
-    "Date",
-    "State_FIPS",
-    "County_FIPS",
-    "Region_Code",
-    "Division_Code",
-    "County",
-    "buildings_1unit",
-    "units_1unit",
-    "permitVal_1unit",
-    "buildings_2units",
-    "units_2units",
-    "permitVal_2units",
-    "buildings_3to4units",
-    "units_3to4units",
-    "permitVal_3to4units",
-    "buildings_5punits",
-    "units_5punits",
-    "permitVal_5punits",
-    "rep_buildings_1unit",
-    "rep_units_1unit",
-    "rep_permitVal_1unit",
-    "rep_buildings_2units",
-    "rep_units_2units",
-    "rep_permitVal_2units",
-    "rep_buildings_3to4units",
-    "rep_units_3to4units",
-    "rep_permitVal_3to4units",
-    "rep_buildings_5punits",
-    "rep_units_5punits",
-    "rep_permitVal_5punits",
-]
-
-all_counties_df_list = []
-
+# Interate through the files and create a dictionary of date: total.
+data = {}
 for file in county_files:
-    all_counties_df_list.append(
-        pd.read_csv(
-            "https://www2.census.gov/econ/bps/County/" + file,
-            dtype={
-                "State_FIPS": str,
-                "County_FIPS": str,
-                "units_1unit": int,
-                "units_2units": int,
-                "units_3to4units": int,
-                "units_5punits": int,
-            },
-            names=header_list,
-            low_memory=False,
-            skiprows=[0, 1],
-        )
-    )
+    url = "https://www2.census.gov/econ/bps/County/" + file
+    with requests.get(url, stream=True, verify=False) as r:
+        lines = (line.decode("utf-8") for line in r.iter_lines())
+        # Skip the first three lines of the file.
+        lines.__next__()
+        lines.__next__()
+        lines.__next__()
 
-all_counties = pd.concat(all_counties_df_list)
-all_counties["FIPS"] = all_counties["State_FIPS"] + all_counties["County_FIPS"]
-region = all_counties.loc[
-    all_counties["FIPS"].isin(
-        ["34005", "34007", "34015", "34021", "42017", "42029", "42045", "42091", "42101"]
-    )
-]
+        for row in csv.reader(lines):
+            # Only include rows that contain non-DVRPC counties.
+            # Ignore data, by line, if there's an error in type or type conversion, but log it.
+            if row[1] + row[2] in [
+                "34005",
+                "34007",
+                "34015",
+                "34021",
+                "42017",
+                "42029",
+                "42045",
+                "42091",
+                "42101",
+            ]:
+                try:
+                    data[row[0]] = (
+                        data[row[0]] + int(row[7]) + int(row[10]) + int(row[13]) + int(row[16])
+                    )
+                except KeyError:
+                    try:
+                        data[row[0]] = int(row[7]) + int(row[10]) + int(row[13]) + int(row[16])
+                    except Exception as e:
+                        logger.error(f"Error in {file} for {row[0]}, {row[1]}{row[2]}: {e}")
+                except Exception as e:
+                    logger.error(f"Error in {file} for {row[0]}, {row[1]}{row[2]}: {e}")
 
-pd.set_option("mode.chained_assignment", None)  # to ignore the copy warning in next step
+# Convert to list, and then the date from YYYY-MM string to proper date.
+data = [[key, value] for key, value in data.items()]
+for each in data:
+    each[0] = date.fromisoformat(each[0][:4] + "-" + each[0][4:] + "-01")
+data.reverse()
 
-region["total_units"] = (
-    region["units_1unit"]
-    + region["units_2units"]
-    + region["units_3to4units"]
-    + region["units_5punits"]
-)
-region = region[["Date", "total_units"]]
-region = region.groupby("Date").sum()
-
-data = []
-for row in region.itertuples():
-    data.append([date.fromisoformat((str(row[0])[:4] + "-" + str(row[0])[4:] + "-01")), row[1]])
-
-# enter into db or create CSV
+# Enter into db or create CSV.
 if not args.csv:
     try:
         with psycopg.connect(PG_CREDS) as conn:
